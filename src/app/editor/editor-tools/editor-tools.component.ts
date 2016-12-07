3a1a1d823681ac28d48e1b02457744003670b886
James Carlson <jxxcarlson@gmail.com>
2016-12-07 06:19:25 -0500

    Add editor-tools files

---
+++ src/app/editor/editor-tools/editor-tools.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'editor-tools',
+  templateUrl: './editor-tools.component.html',
+  styleUrls: ['./editor-tools.component.css']
+})
+export class EditorToolsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
