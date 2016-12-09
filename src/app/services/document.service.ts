2ae570ed24db3d8040b72976d0240831e9a62581
James Carlson <jxxcarlson@gmail.com>
2016-12-08 23:31:45 -0500

    remove dead code

--- src/app/services/document.service.ts
+++ src/app/services/document.service.ts
@@ -43,14 +43,6 @@ export class DocumentService {
     this.store.dispatch({type: ADD_DOCUMENT, payload: document})
   }
 
-  fetchById(id: number) {
-
-    // return this.store.select( s => s.documents ).filter( obj => obj.id == id )[0]
-    // return this.store.select( s => s.documents ).map( obj => typeof(obj))
-    // return this.store.select( s => s.documents ).find( obj => obj.id == id )
-    // return this.store.select(s => s.documents ).map((docs: Observable<Document>) => docs.find((doc: Document) => doc.id === id))
-
-  }
 
   // Use document ID to get doc from the server, then append to document list
   loadDocument(id: number) {
@@ -107,41 +99,6 @@ export class DocumentService {
 
   }
 
-  //// EDITOR ////
-
-  /*
-
-   console.log('Doc Api, update enter')
-
-   var parameter = JSON.stringify(params);
-
-   if (params['query_string'] != undefined) {
-
-   var url = envService.read('apiUrl') + '/documents/' + params['id'] + '?' + params['query_string']
-   }
-   else {
-
-   var url = envService.read('apiUrl') + '/documents/' + params['id']
-   }
-
-   var options = {headers: {"accesstoken": UserService.accessToken()}}
-
-   $http.post(url, parameter, options)
-   */
-
-  // https://angular.io/docs/ts/latest/guide/server-communication.html
-
-  // console.log(`2. Update document ${document.id}`)
-
-  /*
-   var _params = {
-   id: scope.id,
-   title: scope.editableTitle,
-   public: scope.statusPublic,
-   text: scope.editText,
-   author_name: this.document().author
-   }
-   */
 
   updateDocument(document: Document, token: string) {
 
