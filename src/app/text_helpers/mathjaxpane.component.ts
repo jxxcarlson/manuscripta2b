57f320f24301b5de0a310421381a0f55d1b53097
James Carlson <jxxcarlson@gmail.com>
2016-12-12 20:25:21 -0500

    working on mathpane css problem

--- src/app/text_helpers/mathjaxpane.component.ts
+++ src/app/text_helpers/mathjaxpane.component.ts
@@ -4,11 +4,12 @@ import { Document } from '../shared/document.model';
 @Component({
   selector: 'mathjax-pane',
   template: '<div class="rendered-document"  [mathJax]=document.rendered_text>{{document.rendered_text}}</div>',
-  // styleUrls: ['./anypane.component.css', '../vendor/foundation-potion.css' , '../vendor/extras.css', '../vendor/coderay.css']
-  styleUrls: ['./anypane.component.css']
+  styleUrls: ['./asciidoctor.css', './coderay.css', './extras.css', './anypane.component.css']
 })
 export class MathJaxPane {
   @Input() document:Document;
 
 }
 
+
+//  styleUrls: ['./asciidoctor.css', './coderay.css', './extras.css', './anypane.component.css']
