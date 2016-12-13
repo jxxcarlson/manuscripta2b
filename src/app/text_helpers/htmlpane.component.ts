1c639bcf7d219584d3351fce953e82784008cb29
James Carlson <jxxcarlson@gmail.com>
2016-12-12 22:25:38 -0500

    Use asciidoctor.css in mathjaxpane -- for now with encapsulation: ViewEncapsulation.None - maybe ViewEncapsulation.Native later

--- src/app/text_helpers/htmlpane.component.ts
+++ src/app/text_helpers/htmlpane.component.ts
@@ -4,13 +4,19 @@ import { Document } from '../shared/document.model';
 @Component({
   selector: 'html-pane',
   template: '<div class="rendered-document" [innerHTML]=document.rendered_text></div>',
-  styles: [`.rendered-document {
-    font-size: 1.25rem;
-    height: calc(100% - 0px);
-    overflow: scroll;
-}`]
+  styleUrls: ['./asciidoctor.css', './coderay.css', './extras.css', './anypane.component.css']
 })
 export class HtmlPane {
   @Input() document:Document;
 
 }
+/**
+
+
+ styles: [`.rendered-document {
+    font-size: 1.25rem;
+    height: calc(100% - 0px);
+    overflow: scroll;
+}`]
+
+ **/
--- src/app/text_helpers/mathjaxpane.component.ts
+++ src/app/text_helpers/mathjaxpane.component.ts
@@ -1,10 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { Document } from '../shared/document.model';
 
 @Component({
   selector: 'mathjax-pane',
-  template: '<div class="rendered-document"  [mathJax]=document.rendered_text>{{document.rendered_text}}</div>',
-  styleUrls: ['./asciidoctor.css', './coderay.css', './extras.css', './anypane.component.css']
+  template: `
+<div class="rendered-document"  [mathJax]=document.rendered_text>{{document.rendered_text}}</div>`,
+  styleUrls: ['./asciidoctor.css', './coderay.css', './extras.css', './anypane.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class MathJaxPane {
   @Input() document:Document;
